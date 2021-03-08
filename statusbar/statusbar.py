40a48b7d63e41c5793cf403154f31337f3214841
Whom-m0rty <m0rtydisg@gmail.com>
2021-03-08 20:58:25 +0300

    time

--- statusbar/statusbar.py
+++ statusbar/statusbar.py
@@ -3,6 +3,6 @@ from PIL import ImageFont
 
 
 def show_status_bar(draw):
-    font = ImageFont.truetype("arial.ttf", 7)
+    font = ImageFont.truetype("arial.ttf", 9)
     time = get_time_string()
     draw.text((0, -2), time,  font=font, fill=255)
